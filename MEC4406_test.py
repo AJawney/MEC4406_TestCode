1134919c3e109ad0e2d3600b0a7c249d738398d9
AJawney <andrewjawney@gmail.com>
2022-07-13 20:43:02 +1000

    2nd commit

--- MEC4406_test.py
+++ MEC4406_test.py
@@ -1,5 +1,5 @@
 # Print name:
-print("Andrew Jawney")
+print("Yenwaj Werdna")
 
 # Count to favorite number squared:
 favNum = 15
@@ -7,3 +7,19 @@ Numsqrd = favNum**2
 
 for i in range(0,Numsqrd+1):
     print(i)
+
+# Declare object:
+class Engineers:
+    def __init__(self,Name,Field,Years):
+        self.Name = Name
+        self.Field = Field
+        self.Years = str(Years)
+        self.Skill = "problem solver"
+
+# Create objects (Engineers)
+eng1 = Engineers("Andrew", "Mechatronic", 5)
+eng2 = Engineers("Brian", "Agricultural", 7)
+
+# Print out Engineer attributes:
+print(vars(eng1))
+print(vars(eng2))
